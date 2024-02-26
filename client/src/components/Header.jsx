223fadc6f06acb88a6f54b4a51a7846b1f100c9b
Aditya10403 <adityashukla@gmail.com>
2024-02-26 17:27:18 +0530

    complete sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,35 +4,35 @@ import logo from "./logo.png"
 
 export default function Header() {
   return (
-    <header>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3 bg-transparent font-funtext'>
+    <header className='w-full'>
+        <div className='flex justify-between items-center mx-auto p-3 bg-[#d48166] font-funtext shadow-md'>
         <Link to={'/'}>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <img src={logo} alt={"logo"}
             className='w-[60px] h-[35px] sm:w-[150px] sm:h-[60px]'/>
         </h1>
         </Link>
-        <form className='bg-white sm:m-auto ml-3 px-3 py-2 sm:px-4 sm:py-3 rounded-3xl flex items-center text-[#f64c72]'>
+        <form className='bg-white sm:m-auto ml-3 px-3 py-2 sm:px-4 sm:py-3 rounded-3xl flex items-center text-[#d48166]'>
         <input
-        className="bg-transparent focus:outline-none w-24 lg:w-72 placeholder:text-[#f64c72] text-sm placeholder:text-sm"
+        className="bg-transparent focus:outline-none w-24 lg:w-72 placeholder:text-[#d48166] text-sm placeholder:text-sm"
         type="text"
         placeholder="Search..."
        />
-       <FaSearch className='text-[#f64c72]' />
+       <FaSearch className='text-[#d48166]' />
         </form>
         <ul className='flex gap-4'>
             <Link to={'/home'}>
-            <li className='hidden sm:inline active:bg-[#f64c72] active:text-[#2f2fa2] transition-all ease-in 2s font-mono border-2 border-[#f64c72] bg-[#2f2fa2] px-3 py-2 text-xs text-[#f64c72]'>
+            <li className='hidden sm:inline active:bg-white active:text-[#d48166] transition-all ease-in 2s font-mono border-2 border-white bg-[#d48166] px-3 py-2 text-xs text-white'>
                 HOME
             </li>
             </Link>
             <Link to={'/about'}>
-            <li className='hidden sm:inline bg-[#2f2fa2] active:bg-[#f64c72] active:text-[#2f2fa2] transition-all ease-in 2s font-mono border-2 px-3 py-2 text-[#f64c72] text-xs border-[#f64c72]'>
+            <li className='hidden sm:inline bg-[#d48166] active:bg-white active:text-[#d48166] transition-all ease-in 2s font-mono border-2 px-3 py-2 text-white text-xs border-white'>
                 ABOUT
             </li>
             </Link>
             <Link to={'/sign-in'}>
-            <li className='sm:inline bg-[#2f2fa2] active:bg-[#f64c72] active:text-[#2f2fa2] transition-all ease-in 2s font-mono border-2 border-[#f64c72] p-1 text-xs sm:px-3 sm:py-2 text-[#f64c72]'>
+            <li className='sm:inline bg-[#d48166] active:bg-white active:text-[#d48166] transition-all ease-in 2s font-mono border-2 border-white p-1 text-xs sm:px-3 sm:py-2 text-white'>
                 SIGN IN
             </li>
             </Link>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,131 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { ArrowRight} from "lucide-react";
+
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <section className="mx-3 mt-14 mb-5 flex bg-[#373a36] rounded-xl shadow-xl overflow-hidden md:mx-auto max-w-sm lg:max-w-4xl font-normaltext">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="h-full w-full">
+          <img
+            className="mx-auto h-full w-full rounded-md rounded-b-none md:rounded-r-none object-cover"
+            src="https://images.unsplash.com/photo-1546514714-df0ccc50d7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80"
+            alt=""
+          />
+        </div>
+        <div className="flex items-center justify-center px-4 py-10 sm:px-2 sm:py-4 lg:px-1 lg:py-8">
+          <div className="sm:w-[90%] md:w-[80%]">
+            <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl">
+              Sign up
+            </h2>
+            <p className="mt-2 pt-2 text-white text-sm">
+              Already have an account?{" "}
+              <Link to={'/sign-in'}>
+              <span className="font-medium text-[#e6e2dd] text-sm transition-all duration-200 font-funtext hover:underline"
+              >
+                Sign In
+              </span>
+              </Link>
+            </p>
+            <form action="#" method="POST" className="mt-8">
+              <div className="space-y-5">
+                <div>
+                  <label
+                    htmlFor="name"
+                    className="text-base font-medium text-white"
+                  >
+                    {" "}
+                    User Name{" "}
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      className="flex h-10 w-full rounded-md border border-[#e6e2dd] bg-white px-3 py-2 text-sm placeholder:text-[#e6e2dd] focus:outline-none focus:ring-1 focus:ring-[#e6e2dd] focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      type="text"
+                      placeholder="username"
+                      id="username"
+                    ></input>
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="text-base font-medium text-white"
+                  >
+                    {" "}
+                    Email address{" "}
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      className="flex h-10 w-full rounded-md border border-[#e6e2dd] bg-white px-3 py-2 text-sm placeholder:text-[#e6e2dd] focus:outline-none focus:ring-1 focus:ring-[#e6e2dd] focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      type="email"
+                      placeholder="email"
+                      id="email"
+                    ></input>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="password"
+                      className="text-base font-medium text-white"
+                    >
+                      {" "}
+                      Password{" "}
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      className="flex h-10 w-full rounded-md border border-[#e6e2dd] bg-white px-3 py-2 text-sm placeholder:text-[#e6e2dd] focus:outline-none focus:ring-1 focus:ring-[#e6e2dd] focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      type="password"
+                      placeholder="password"
+                      id="password"
+                    ></input>
+                  </div>
+                </div>
+                <div>
+                  <button
+                    type="button"
+                    className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80
+                    active:bg-black/50"
+                  >
+                    Create Account <ArrowRight className="ml-2" size={16} />
+                  </button>
+                </div>
+              </div>
+            </form>
+            <div className="mt-3 space-y-3">
+              <button
+                type="button"
+                className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none
+                active:bg-white/60"
+              >
+                <span className="mr-2 inline-block">
+                  <svg class="h-6 w-6" viewBox="0 0 40 40">
+                    <path
+                      d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                      fill="#FFC107"
+                    />
+                    <path
+                      d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
+                      fill="#FF3D00"
+                    />
+                    <path
+                      d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
+                      fill="#4CAF50"
+                    />
+                    <path
+                      d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                      fill="#1976D2"
+                    />
+                  </svg>
+                </span>
+                Sign up with Google
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
