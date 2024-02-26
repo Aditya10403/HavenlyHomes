530ef735cc1ca2fc42ee334c2baade12524f1ac0
Aditya10403 <adityashukla@gmail.com>
2024-02-26 08:56:34 +0530

    create and run server with express

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,34 +5,34 @@ import logo from "./logo.png"
 export default function Header() {
   return (
     <header>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3 bg-transparent'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3 bg-transparent font-funtext'>
         <Link to={'/'}>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <img src={logo} alt={"logo"}
-            className='w-[80px] h-[60px]'/>
+            className='w-[60px] h-[35px] sm:w-[150px] sm:h-[60px]'/>
         </h1>
         </Link>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form className='bg-white sm:m-auto ml-3 px-3 py-2 sm:px-4 sm:py-3 rounded-3xl flex items-center text-[#f64c72]'>
         <input
-        className="bg-transparent focus:outline-none w-24 sm:w-64"
+        className="bg-transparent focus:outline-none w-24 lg:w-72 placeholder:text-[#f64c72] text-sm placeholder:text-sm"
         type="text"
         placeholder="Search..."
        />
-       <FaSearch className='text-slate-600' />
+       <FaSearch className='text-[#f64c72]' />
         </form>
-        <ul className='flex'>
+        <ul className='flex gap-4'>
             <Link to={'/home'}>
-            <li className='hidden sm:inline active:bg-[#8e8d8a] transition-all ease-in 2s font-mono border border-[#8e8d8a] px-3 py-2 text-black border-r-0'>
+            <li className='hidden sm:inline active:bg-[#f64c72] active:text-[#2f2fa2] transition-all ease-in 2s font-mono border-2 border-[#f64c72] bg-[#2f2fa2] px-3 py-2 text-xs text-[#f64c72]'>
                 HOME
             </li>
             </Link>
             <Link to={'/about'}>
-            <li className='hidden sm:inline  active:bg-[#8e8d8a] transition-all ease-in 2s font-mono border border-[#8e8d8a] px-3 py-2 text-black'>
+            <li className='hidden sm:inline bg-[#2f2fa2] active:bg-[#f64c72] active:text-[#2f2fa2] transition-all ease-in 2s font-mono border-2 px-3 py-2 text-[#f64c72] text-xs border-[#f64c72]'>
                 ABOUT
             </li>
             </Link>
             <Link to={'/sign-in'}>
-            <li className='sm:inline active:bg-[#8e8d8a] transition-all ease-in 2s font-mono border border-[#8e8d8a] px-3 py-2 text-black border-l-0'>
+            <li className='sm:inline bg-[#2f2fa2] active:bg-[#f64c72] active:text-[#2f2fa2] transition-all ease-in 2s font-mono border-2 border-[#f64c72] p-1 text-xs sm:px-3 sm:py-2 text-[#f64c72]'>
                 SIGN IN
             </li>
             </Link>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,10 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      normaltext: ["Poppins", "sans-serif"],
+      funtext: ["Kode Mono", "monospace"],
+    },
     extend: {},
   },
   plugins: [],
